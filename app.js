192257b5fc910c8f9a46c295fa00e9f5619f6623
Ryuhwa <im706374@gmail.com>
2024-06-14 13:00:54 +0900

    profile 배열 생성

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+//! 비동기식 호출을 위한 배열 생성
+const test = (backnumber, title, position) =>{
+  const profile = {
+    userId: backnumber,
+    title: title,
+    body: position,
+  }
+}
+
+//! jsonplaceholder 불러오기
+fetch('https://jsonplaceholder.typicode.com/posts'), {
+  method: 'post',
+  body: JSON.stringify(profile),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+}
+//! 출력 코드
   .then((response) => response.json())
   .then((json) => console.log(json));
